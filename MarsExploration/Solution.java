5dbe1b7c214afb955affefca9087df71c33cdb36
Luiz Ricardo de Lima <luizricardodelima9@gmail.com>
2021-09-30 07:11:49 -0300

    Solved MarsExploration

---
+++ MarsExploration/Solution.java
@@ -0,0 +1,54 @@
+package MarsExploration;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Result {
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(String s) {
+    // Write your code here
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            switch (i % 3) {
+                case 0:
+                case 2:
+                    if (s.charAt(i) != 'S') count++;
+                    break;
+                case 1:
+                    if (s.charAt(i) != 'O') count++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out)); //new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
